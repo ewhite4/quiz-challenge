93147ebccb14adf1bba78775a1a84efa8e68f033
ewhite <ew117mc@yahoo.com>
2022-02-26 09:28:50 -0700

    css changes

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ var stopInterval = function() {
     console.log('time is up');
     clearInterval(timer);
 }
+
+var questions = [
+    {
+        prompt: "What is JavaScript?\n(1) coding\n\(2) cooking\(c) construction",
+        answer: "a"
+    }
+]
+
+var questions = []
+var score = 0;
+
+for(var i=0; i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+        score++;
+        alert("Correct!");
+    } else {
+        alert("Wrong!");
+    }
+}
+alert("Score " + score + "/" + questions.length);
